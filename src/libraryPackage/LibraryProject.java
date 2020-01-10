676c6d033322d673ad80899c1820e229d9d87ac3
Ivaylo Ivanov <ivailorozenov@abv.bg>
2020-01-11 01:44:32 +0200

    Edited some text

--- src/libraryPackage/LibraryProject.java
+++ src/libraryPackage/LibraryProject.java
@@ -9,7 +9,8 @@ public class LibraryProject {
 		System.out.println("Име: " + myLibrary.getLibraryList().get(0).getBookName());
 		myLibrary.insertNewBook("Доктор Сън", "Стивън Кинг", "Плеяда","9789544093440", 2014);
 		System.out.println("Име: " + myLibrary.getLibraryList().get(1).getBookName());
-
+		myLibrary.insertNewBook("Преобърнатият човек", "Фред Варгас", "Колибри","9786190205760", 2020);
+		System.out.println("Име: " + myLibrary.getLibraryList().get(2).getBookName());
 
 	}
 }
