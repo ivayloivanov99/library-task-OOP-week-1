39d25a70024f0930edc7a07cd4d6ed695a3ba0e8
Ivaylo Ivanov <ivailorozenov@abv.bg>
2020-01-11 14:10:46 +0200

    Finished the project

--- src/libraryPackage/Library.java
+++ src/libraryPackage/Library.java
@@ -17,14 +17,52 @@ public class Library extends Book{
 		this.libraryName = libraryName;
 		this.libraryList = libraryList;
 	}
-
-	public void insertNewBook(String name, String author, String isbn, String publisher, int year) {			
+	
+	//Добавяне на нова книга
+	public void insertNewBook(String name, String author, String publisher, String isbn, int year) {			
 			
 		Book newBook = new Book(name,author,publisher,year,isbn);
 		libraryList.add(newBook);
 		
 	}
 	
+	//Търсене на книга по предварително зададен автор
+	public void searchBookAuthor(String author) {
+		System.out.println("\n===== Книги с автор: " + author + " =====\n");
+		for(int i =0; i < libraryList.size();i++) {
+			if(author == libraryList.get(i).getBookAuthor()) {
+				System.out.println("Име: "+ libraryList.get(i).getBookName() + "  | Автор: " 
+									+ libraryList.get(i).getBookAuthor() + "  | Издател: " 
+									+ libraryList.get(i).getBookPublisher() + "  | Година на издаване: " 
+									+ libraryList.get(i).getBookYear() + "  | ISBN номер: " 
+									+ libraryList.get(i).getBookISBN() + "\n");
+			
+			}	
+		}
+	}
+	
+	//Извеждане на информация за всички книги
+	public void displayAllBooks() {
+		System.out.println("\n===== Всички книги в библиотеката =====\n");
+		for(int i =0; i < libraryList.size();i++) {
+				System.out.println("Име: "+ libraryList.get(i).getBookName() + "  | Автор: " 
+									+ libraryList.get(i).getBookAuthor() + "  | Издател: " 
+									+ libraryList.get(i).getBookPublisher() + "  | Година на издаване: " 
+									+ libraryList.get(i).getBookYear() + "  | ISBN номер: " 
+									+ libraryList.get(i).getBookISBN()+ "\n");
+		    }
+		}
+	
+	//Изтриване на книга от библиотеката
+	public void deleteBook(String author) {
+		System.out.println("\n!=====! Изтриване на книги с автор: " + author + " !=====!\n");
+		for(int i =0; i < libraryList.size();i++) {
+			if(author == libraryList.get(i).getBookAuthor()) {
+				libraryList.remove(i);
+				}
+			}	
+	}
+		
 	public String getLibraryName() {
 		return libraryName;
 	}
--- src/libraryPackage/LibraryProject.java
+++ src/libraryPackage/LibraryProject.java
@@ -5,12 +5,21 @@ public class LibraryProject {
 	public static void main(String[] args) {	
 		Library myLibrary = new Library();
 
-		myLibrary.insertNewBook("Другият", "Стивън Кинг", "Плеяда","9789544093976", 2019);
-		System.out.println("Име: " + myLibrary.getLibraryList().get(0).getBookName());
-		myLibrary.insertNewBook("Доктор Сън", "Стивън Кинг", "Плеяда","9789544093440", 2014);
-		System.out.println("Име: " + myLibrary.getLibraryList().get(1).getBookName());
+		myLibrary.insertNewBook("Другият", "Стивън Кинг", "Плеяда","9789544093976", 2019);	
 		myLibrary.insertNewBook("Преобърнатият човек", "Фред Варгас", "Колибри","9786190205760", 2020);
-		System.out.println("Име: " + myLibrary.getLibraryList().get(2).getBookName());
-
+		myLibrary.insertNewBook("Хладнокръвно", "Труман Капоти", "Колибри","9786190205708", 2020);
+		myLibrary.insertNewBook("Тайната на черната скала", "Джо Тод-Стантън", "Дакелче","9786199105351", 2017);
+		myLibrary.insertNewBook("Доктор Сън", "Стивън Кинг", "Плеяда","9789544093440", 2014);
+		myLibrary.insertNewBook("Щипка вкус", "Кейт Алинсън", "Книгомания","9786191952427", 2015);
+		myLibrary.insertNewBook("Проклятието", "Стивън Кинг", "Бард","9789545859540", 2018);
+		myLibrary.insertNewBook("Милионерът в Лисабон", "Жозе Родригеш душ Сантуш", "Хермес","9789542617471", 2019);	
+		myLibrary.insertNewBook("Лятно пътуване", "Труман Капоти", "Колибри","9786190203971", 2019);
+		
+		myLibrary.displayAllBooks();
+		myLibrary.searchBookAuthor("Труман Капоти");
+		myLibrary.deleteBook("Стивън Кинг");
+		myLibrary.displayAllBooks();
+		
+		
 	}
 }
